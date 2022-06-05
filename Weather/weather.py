218828cf398e9d846703d6be30fc549d634eafbc
MarseusFu <marseus891130@gmail.com>
2022-06-05 12:34:38 +0800

    註解

--- Weather/weather.py
+++ Weather/weather.py
@@ -9,8 +9,8 @@ def getWeather(url):
     for i in location:
         city = i['locationName']    # 縣市名稱
         wx8 = i['weatherElement'][0]['time'][0]['parameter']['parameterName']    # 天氣現象
-        maxt8 = i['weatherElement'][1]['time'][0]['parameter']['parameterName']  # 最低溫
-        mint8 = i['weatherElement'][2]['time'][0]['parameter']['parameterName']  # 最高溫
+        maxt8 = i['weatherElement'][1]['time'][0]['parameter']['parameterName']  # 最高溫
+        mint8 = i['weatherElement'][2]['time'][0]['parameter']['parameterName']  # 最低溫
         ci8 = i['weatherElement'][2]['time'][0]['parameter']['parameterName']    # 舒適度
         pop8 = i['weatherElement'][2]['time'][0]['parameter']['parameterName']   # 降雨機率
         print(f'{city}未來 8 小時{wx8}，最高溫 {maxt8} 度，最低溫 {mint8} 度，降雨機率 {pop8} %')
