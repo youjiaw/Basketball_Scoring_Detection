b6df721c5eece9750a9a3c7a16e68cec51f0b294
Webber29 <77779576+Webber29@users.noreply.github.com>
2022-06-11 19:14:05 +0800

    shot detection code

--- final.py
+++ final.py
@@ -1,33 +1,35 @@
-from django.conf import settings
 import cv2
+import time
+import sys
+import traceback
 import os
-
+import tqdm
 
 # ----------modify input video here----------
-input_video_dir = settings.MEDIA_ROOT
-video_name = ""
+input_video_dir = "/Users/apple/Desktop/專題/籃球/training and testing datas/final_test"
+video_name = "final_test" + ".mp4"
 
 # ----------modify ouput highlight video here----------
-highlight_saving_dir = input_video_dir +"/"
+highlight_saving_dir = "/Users/apple/Desktop/"
 
 # ----------modify cfg and weight file here----------
 # basketball
-CONFIG_b = input_video_dir +'/../video_handler' + '/model_datas/basketball_data/cfg/yolov4-basketball.cfg'
-WEIGHT_b = input_video_dir +'/../video_handler' + '/model_datas/basketball_data/weights/yolov4-basketball_best_2.weights'
-class_dir_b = input_video_dir +'/../video_handler' + "/model_datas/basketball_data/obj.names"
+CONFIG_b = 'model_datas/basketball_data/cfg/yolov4-basketball.cfg'
+WEIGHT_b = 'model_datas/basketball_data/weights/yolov4-basketball_best_2.weights'
+class_dir_b = "model_datas/basketball_data/obj.names"
 # shot
-CONFIG_s = input_video_dir +'/../video_handler' + '/model_datas/shot_data/cfg/yolov4-shot.cfg'
-WEIGHT_s = input_video_dir +'/../video_handler' + '/model_datas/shot_data/weights/yolov4-shot_best.weights'
-class_dir_s = input_video_dir +'/../video_handler' + "/model_datas/shot_data/obj.names"
+CONFIG_s = 'model_datas/shot_data/cfg/yolov4-shot.cfg'
+WEIGHT_s = 'model_datas/shot_data/weights/yolov4-shot_best.weights'
+class_dir_s = "model_datas/shot_data/obj.names"
 
 # --------------------paramaters--------------------
 # initial skip frames
 frame_batch_init = 10
 
-shot_start_indexes = []
-score_indexes = []
+score_indexes = []#
+shot_start_indexes = []#
+shot_start_frame = 0
 
-shot_start_frame_index = 0
 is_shooting = 0
 is_score_f = 0
 scoring_frames = 0
@@ -41,11 +43,8 @@ checking_times = 3
 shot_end_check = checking_times
 frame_batch = frame_batch_init
 previous_ball_height = 0
-ball_overlapping_hoop = 0
+ball_overlapping_hoop = 0;
 ball_is_dropping = 0
-
-scores = 0
-misses = 0
 
 # 讀取模型與訓練權重         
 def initNet():
@@ -98,22 +97,18 @@ def Detect_Basketball(classes, confs, boxes):
                             shot_end_check -= 1
                         else:
                             global scoring_frames
-                            global shot_start_frame_index
+                            global shot_start_frame
 
                             if scoring_frames >= scoring_frames_target:
-                                global is_score_f, scores
+                                global is_score_f
                                 is_score_f = 1
-                                scores += 1
-                                shot_start_indexes.append(shot_start_frame_index)
+                                shot_start_indexes.append(shot_start_frame)
                                 # print("score!")
-                            else:
-                                global misses
-                                misses += 1
                                 
                             global frame_batch
                             global ball_is_dropping
                             ball_is_dropping = 1
-                            shot_start_frame_index = 0
+                            shot_start_frame = 0
                             scoring_frames = 0
                             is_shooting = 0
                             frame_batch = frame_batch_init
@@ -153,6 +148,8 @@ def save_highlight_frames():
 
     if score_count<=0:
         return
+    
+    print("The video has ",score_count," scoring shots.")
         
     video_path = input_video_dir + "/" + video_name
     output_path = highlight_saving_dir
@@ -166,29 +163,32 @@ def save_highlight_frames():
     frame_fps = video_capture.get(cv2.CAP_PROP_FPS)
     frame_final = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
 
-    fourcc = cv2.VideoWriter_fourcc(*"avc1")
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     out_video_path = os.path.join(output_path, output_video_name)
-    print("in:",video_path)
-    print("out:",out_video_path)
-
+    # print(out_video_path)
     label_video_writer = cv2.VideoWriter(out_video_path, fourcc, frame_fps, (frame_w, frame_h))
 
     frame_init = 0
     frame_length = frame_final - frame_init
 
+    print('start makeing highlight video    ')
+    print("start: ",shot_start_indexes)
+    print("end  : ",score_indexes)
+    pbar = tqdm.tqdm(total=frame_length, mininterval=0.001)
+
     target = 0
     frames_before_hoop_height = int(frame_fps)
+    print("frames_before_hoop_height:",frames_before_hoop_height)
     if frames_before_hoop_height >= shot_start_indexes[0]:
         target = score_indexes[0]
         
     last_score_end = score_indexes[score_count-1]
 
-    first = 1
-
     # start reading video
     try:
         while True:
             ret, img = video_capture.read()
+            pbar.update(1)
             if ret != True:
                 break;
             frame_index = int(video_capture.get(cv2.CAP_PROP_POS_FRAMES))
@@ -205,48 +205,43 @@ def save_highlight_frames():
             if frame_index <= target:
                 label_video_writer.write(img)
 
-            # write first frame
-            if first:
-                p = os.path.join(input_video_dir, 'filter_imgs/origin.png')
-                print("img:",p)
-                cv2.imwrite(p, img)
-                first = 0
-
     except:
         pass
     
     finally:
-        # os.system("ffmpeg -i " + video_name + " -vcodec libx264 H264_" + video_name)
+        pbar.close()
         video_capture.release()
         label_video_writer.release()
        
-def main(filename, targetdir):
-    global video_name, highlight_saving_dir, shot_start_frame_index, score_indexes, is_score_f
-    video_name = filename
-    highlight_saving_dir += targetdir
+if __name__ == '__main__':
+    
+    start = time.time()
 
     video_path = input_video_dir + "/" + video_name
     video_capture = cv2.VideoCapture(video_path)
     video_capture.set(1, 0)
-
+    
     frame_final = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
+    print('-----------------------------------')
+    print('      start    ')
+    print('-----------------------------------')
+    print('process frame length: ', frame_final)
+
+    pbar = tqdm.tqdm(total=frame_final, mininterval=0.001)
 
     model_b, model_s = initNet()
+    success = fail = 0
+    number = 1
+
     skip_count = 0
-    counter = 0
     # start reading video
     try:
         while True:
-
-            counter += 1
-            if counter > frame_final:
-                # print("Reach setting final frame")
-                break;
             ret, img = video_capture.read()
-
+            pbar.update(1)
             if ret != True:
-                # break;
-                continue
+                # print("Read video error")
+                break;
 
             frame_index = int(video_capture.get(cv2.CAP_PROP_POS_FRAMES))
 
@@ -264,30 +259,47 @@ def main(filename, targetdir):
             classes, confs, boxes = model_b.detect(img, 0.4, 0.1)
             classes_s, confs_s, boxes_s = model_s.detect(img, 0.4, 0.1)
 
-            if len(boxes) != 0: 
+            if len(boxes) == 0:  # detect none
+                fail += 1
+            else:
+                success += 1
                 Detect_Basketball(classes, confs, boxes)
 
+            # draw shot result
             if len(boxes_s) != 0:
                 Detect_Hoop(classes_s, confs_s, boxes_s)
 
-            if previous_ball_height < h3 and shot_start_frame_index==0:
-                shot_start_frame_index = frame_index
-
+            number += 1
+            
+            if previous_ball_height < h3 and shot_start_frame==0:
+                shot_start_frame = frame_index
+            
             if is_score_f:
                 score_indexes.append(frame_index)
-                shot_start_frame_index = 0
+                shot_start_frame = 0
                 is_score_f = 0;
-    except:
-        pass
-     
+
+    except Exception as e:
+        error_class = e.__class__.__name__  # 取得錯誤類型
+        detail = e.args[0]  # 取得詳細內容
+        cl, exc, tb = sys.exc_info()  # 取得Call Stack
+        lastCallStack = traceback.extract_tb(tb)[-1]  # 取得Call Stack的最後一筆資料
+        line_num = lastCallStack[1]  # 取得發生的行號
+        func_name = lastCallStack[2]  # 取得發生的函數名稱
+        error_infor = "line {}, in {}: [{}] {}".format(line_num, func_name, error_class, detail)
+        print(detail)
+        print(error_infor)
+
     finally:
+        pbar.close()
         video_capture.release()
+
+    print('※ {} 張內， 辨識{} 張、省略 {} 張'.format(frame_final, number, frame_final-number))
+    end = time.time()
+    print("執行時間：%f 分" % float((end - start) / 60))
     
-    print("end!")
-    print("shot_start_indexes:",shot_start_indexes,", ","score_indexes:",score_indexes)
     
     # save shot list and making highlight video
     save_highlight_frames()
 
-    return (scores, misses)
-
+    
