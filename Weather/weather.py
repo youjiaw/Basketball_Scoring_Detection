391179f8eda0619cb5da38c12286ef3739fe4d7b
MarseusFu <marseus891130@gmail.com>
2022-06-13 16:45:20 +0800

    update pm25 crawl

--- Weather/weather.py
+++ Weather/weather.py
@@ -21,14 +21,14 @@ def getPM2dot5(pm_url):
     data_json = data.json()
     records = data_json['records']
     for i in records:
-        site = i['site']
+        site = i['sitename']
         county = i['county']
-        pm25 = i['pm25']
-        time = i['datacreationdate']
+        pm25 = i['pm2.5']
+        time = i['publishtime']
         print(f'{county} {site}，pm2.5數值{pm25}，資料時間 {time}')
     
 if __name__=="__main__":
     url = 'https://opendata.cwb.gov.tw/fileapi/v1/opendataapi/F-C0032-001?Authorization=CWB-A89DDEBE-215B-4AE4-AA1B-7A7F52C58235&downloadType=WEB&format=JSON'
     getWeather(url)
-    pm_url = 'https://data.epa.gov.tw/api/v2/aqx_p_02?api_key=fdac918f-b34b-4dd6-94b2-338ea65d1a3f&format=json'
+    pm_url = 'https://data.epa.gov.tw/api/v2/aqx_p_432?api_key=fdac918f-b34b-4dd6-94b2-338ea65d1a3f&format=json'
     getPM2dot5(pm_url)   
